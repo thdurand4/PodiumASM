7fe1ade08093d9615a28cbed112fde66793f8fc8
thdurand4 <91953714+thdurand4@users.noreply.github.com>
2022-12-05 15:42:06 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -61,14 +61,14 @@ master_doc = 'index'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 html_theme = 'sphinx_rtd_theme'
-
+html_css_files = ["theme.css"]
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
-    'analytics_id': 'UA-172723859-1',  #  Provided by Google in your dashboard
+    #'analytics_id': 'UA-172723859-1',  #  Provided by Google in your dashboard
     'analytics_anonymize_ip': False,
-    'logo_only': False,
+    'logo_only': True,
     'display_version': True,
     'prev_next_buttons_location': 'bottom',
     'style_external_links': False,
